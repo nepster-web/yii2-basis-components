be906e85ce4eeb8c1bfa930d4c5bc5faabd4a81d
Nepster <nepsterxxx@mail.ru>
2015-08-08 18:13:47 +0300

    update lang

--- messages/en/basis.php
+++ messages/en/basis.php
@@ -2,4 +2,113 @@
 
 return [
 
+    // Общее
+    'ago' => '',
+    'and' => '',
+    'or' => '',
+
+    'minus' => '',
+    'plus' => '',
+
+    // Наименование цифр
+    'zero' => '',
+    'one' => '',
+    'two' => '',
+    'three' => '',
+    'four' => '',
+    'five' => '',
+    'six' => '',
+    'seven' => '',
+    'eight' => '',
+    'nine' => '',
+    'ten' => '',
+    'eleven' => '',
+    'twelve' => '',
+    'thirty' => '',
+    'fourteen' => '',
+    'fifteen' => '',
+    'sixteen' => '',
+    'seventeen' => '',
+    'eighteen' => '',
+    'nineteen' => '',
+    'twenty' => '',
+    'thirteen' => '',
+    'forty' => '',
+    'fifty' => '',
+    'sixty' => '',
+    'seventy' => '',
+    'eighty' => '',
+    'ninety' => '',
+    'hundred' => '',
+    'thousand' => '',
+    'million' => '',
+    'billion' => '',
+    'trillion' => '',
+    'quadrillion' => '',
+    'quintillion' => '',
+    'sextillion' => '',
+    'septillion' => '',
+    'octillion' => '',
+    'nonillion' => '',
+    'detsillion' => '',
+
+    // Мера времени
+    'week' => '',
+    'second' => '',
+    'minute' => '',
+    'day' => '',
+    'hour' => '',
+    'month' => '',
+    'year' => '',
+
+    // Время года
+    'January' => '',
+    'February' => '',
+    'March' => '',
+    'April' => '',
+    'May' => '',
+    'June' => '',
+    'July' => '',
+    'August' => '',
+    'September' => '',
+    'October' => '',
+    'November' => '',
+    'December' => '',
+
+    // Дни недели
+    'Monday' => '',
+    'Tuesday' => '',
+    'Wednesday' => '',
+    'Thursday' => '',
+    'Friday' => '',
+    'Saturday' => '',
+    'Sunday' => '',
+
+    // Время
+    '{n} sec.' => '',
+    '{n} min.' => '',
+    '{n} h.' => '',
+    '{n} d.' => '',
+    '{n} m.' => '',
+    '{n} y.' => '',
+
+    'a second ago' => '',
+    'a minute ago' => '',
+    'a hour ago' => '',
+    'a day ago' => '',
+    'a year ago' => '',
+
+    '{n, plural, one{# second} other{# seconds}}' => '',
+    '{n, plural, one{# second} other{# seconds}} ago' => '',
+
+    '{n, plural, one{# minute} other{# minutes}}' => '',
+    '{n, plural, one{# minute} other{# minutes}} ago' => '',
+
+    '{n, plural, one{# hour} other{# hours}}' => '',
+
+    '{n, plural, one{# day} other{# days}}' => '',
+
+    '{n, plural, one{# month} other{# months}}' => '',
+
+    '{n, plural, one{# year} other{# years}}' => '',
 ];
